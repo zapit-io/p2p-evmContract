fa22ca6675d04dc001a80812fea7dff733b300e9
Suranjan <suranjanbiswas2002@gmail.com>
2023-11-17 02:50:01 +0530

    update: updated the code for data

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,19 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const p2pEscrow = await ethers.deployContract("ZapitP2PEscrow", [100]);
-  await p2pEscrow.waitForDeployment();
+  // const p2pEscrow = await ethers.deployContract("ZapitP2PEscrow", [100]);
+  // await p2pEscrow.waitForDeployment();
+
+  // // verify contract on polygonscan on mumbai
+
+  // console.log(`ZapitP2PEscrow deployed to ${p2pEscrow.target}`);
+
+  const signature = await ethers.deployContract("Signature");
+  await signature.waitForDeployment();
 
   // verify contract on polygonscan on mumbai
 
-  console.log(`ZapitP2PEscrow deployed to ${p2pEscrow.target}`);
+  console.log(`Signature deployed to ${signature.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
---
+++ test/Signature.ts
@@ -0,0 +1,52 @@
+import {
+  time,
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("ZapitP2PEscrow", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deploySigner() {
+    // Contracts are deployed using the first signer/account by default
+    const [deployer] = await ethers.getSigners();
+    const MESSAGE = "ABCD";
+
+    const signature = await ethers.deployContract("Signature");
+
+    return {
+      signature,
+      deployer,
+      MESSAGE,
+    };
+  }
+
+  // fixture for using a created-escrow
+
+  describe("Check the verification of the signature", function () {
+    it("Check the signature to be correct", async function () {
+      const { signature, deployer, MESSAGE } = await loadFixture(deploySigner);
+
+      const messageHash = await signature
+        .connect(deployer)
+        .getMessageHash(MESSAGE);
+      const sig = await deployer.signMessage(messageHash);
+      const signData = await signature
+        .connect(deployer)
+        .getEthSignedMessageHash(messageHash);
+      console.log({
+        signData,
+        sig,
+      });
+      const validation = await signature.connect(deployer).verify(MESSAGE, sig);
+
+      console.log({
+        validation,
+      });
+
+      // expect(validation).to.equal(true);
+    });
+  });
+});

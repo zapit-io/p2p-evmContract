4d3f6a5e7572de37bcab2e0de39defd643ad7374
Suranjan <suranjanbiswas2002@gmail.com>
2023-11-20 00:41:50 +0530

    update: updated the docs

--- test/Signature.ts
+++ test/Signature.ts
@@ -6,11 +6,7 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("Signature", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
   async function deploySigner() {
-    // Contracts are deployed using the first signer/account by default
     const [deployer] = await ethers.getSigners();
     const MESSAGE = "ABCD";
 
